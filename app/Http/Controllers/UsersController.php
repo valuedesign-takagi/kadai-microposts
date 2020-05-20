9a981c0c08f49427b2253f22a5f0c6b538119aec
rin.takagi <rin.takagi@techacademy.jp>
2020-05-20 17:44:43 +0000

    post

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -22,11 +22,18 @@ class UsersController extends Controller
     public function show($id)
     {
         // idの値でユーザを検索して取得
-        $user = User::findOrFail($id);
+        $user = User::find($id);
 
-        // ユーザ詳細ビューでそれを表示
+        // 関係するモデルの件数をロード
+        $user->loadRelationshipCounts();
+
+        // ユーザの投稿一覧を作成日時の降順で取得
+        $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+
+        // ユーザ詳細ビューでそれらを表示
         return view('users.show', [
             'user' => $user,
+            'microposts' => $microposts,
         ]);
     }
 }
