96e9e21cc46271a949117f59ea2b339d5a66e134
rin.takagi <rin.takagi@techacademy.jp>
2020-05-27 11:11:10 +0000

    favorite table update3

--- resources/views/user_favorite/favorite_button.blade.php
+++ resources/views/user_favorite/favorite_button.blade.php
@@ -1,4 +1,4 @@
-@if (Auth::id() != $micropost->id)
+@if (Auth::user()->is_favorite($micropost->id))
     {{-- アンフォローボタンのフォーム --}}
     {!! Form::open(['route' => ['favorites.unfavorite', $micropost->id], 'method' => 'delete']) !!}
         {!! Form::submit('Unfavorite', ['class' => "btn btn-danger btn-sm"]) !!}
